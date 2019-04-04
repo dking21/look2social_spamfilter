560aab78df913d72ec99871afd6288e4511c781b
Daniel Kim <dhk2013@gmail.com>
2019-04-03 17:44:25 -0700

    update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -27,3 +27,25 @@ def test_logistic_regressio(data,column):
     print("AUC score of this model is" + "\n")
     print(roc_auc_score(y_test,y_predict))
     return None
+
+def test_model(data,column,model):
+    y = data[column]
+    X = data[['Docusign', 'onespan', 'signnow','adobe sign',
+                           'listed_count', 'statuses_count','followers_count',
+                           'favourites_count', 'friends_count','time_float_sin',
+                           'time_float_cos', 'is_description_none']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    model = model()
+    model.fit(X_train,y_train)
+    y_predict = model.predict(X_test)
+    y_pred = model.predict_proba(X_test)
+
+    print("Accuracy score of this model is" + "\n")
+    print(accuracy_score(y_test,y_predict))
+    print("\n" + "Confusion Matrix of this model is" + "\n")
+    print(confusion_matrix(y_test,y_predict))
+    print("\n" + "Log-Loss score of this model is" + "\n")
+    print(log_loss(y_test,y_pred))
+    print("\n" + "AUC score of this model is" + "\n")
+    print(roc_auc_score(y_test,y_predict))
+    return None
