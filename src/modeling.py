f1e4303508230364e17ac29704414cf40c1e9a0b
Daniel Kim <dhk2013@gmail.com>
2019-04-05 14:28:17 -0700

    modifying modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -8,27 +8,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
-def test_logistic_regressio(data,column):
-    y = data[column]
-    X = data[['Docusign', 'onespan', 'signnow','adobe sign',
-                           'listed_count', 'statuses_count','followers_count',
-                           'favourites_count', 'friends_count','time_float_sin',
-                           'time_float_cos', 'is_description_none']]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-    logistic_model = LogisticRegression()
-    logistic_model.fit(X_train,y_train)
-    y_predict = logistic_model.predict(X_test)
-    y_pred = logistic_model.predict_proba(X_test)
-
-    print("Accuracy score of this model is" + "\n")
-    print(accuracy_score(y_test,y_predict))
-    print("Confusion Matrix of this model is" + "\n")
-    print(confusion_matrix(y_test,y_predict))
-    print("Log-Loss score of this model is" + "\n")
-    print(log_loss(y_test,y_pred))
-    print("AUC score of this model is" + "\n")
-    print(roc_auc_score(y_test,y_predict))
-    return None
 
 def test_model(data,column,model):
     y = data[column]
